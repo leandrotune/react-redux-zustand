4f7fe228e0aca43397a7a21ce5cda7d021a97dbd
leandrotune <leandrotune@pop-os.localdomain>
2023-07-17 18:10:24 -0300

    interface da aplicação

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+      <span>Módulo Desvendado o Redux</span>
+    </header>
+  )
+}
